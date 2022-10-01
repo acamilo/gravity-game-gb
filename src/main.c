2ac5200982fb332c746e0a1aa833b591e77b6d74
Alex Camilo <alex.camilo@gmail.com>
2022-10-01 15:14:19 -0400

    added cam's tiles

--- src/main.c
+++ src/main.c
@@ -54,30 +54,64 @@ uint8_t background_scroll_y = 0;
 uint16_t player_x_pos=20<<8;
 uint16_t player_y_pos=20<<8;
 
-int8_t player_x_vel = 0;
-int8_t player_y_vel = 0;
+int16_t player_y_vel = 0;
+int16_t player_x_vel = 0;
 
-uint8_t player_sprite_x = 0;
-uint8_t player_sprite_y = 0;
+BOOLEAN jumpflag=FALSE;
 
-#define maxvel 32
+#define maxvel 16
 
 void handle_player_sprite_physics(){
-    if (abs(player_y_vel)>maxvel){
-        if(player_y_vel>0) { player_y_vel = maxvel; }
-        else { player_y_vel = -maxvel; }
-    }
-        if (abs(player_x_vel)>maxvel){
-        if(player_x_vel>0) { player_x_vel = maxvel; }
-        else { player_x_vel = -maxvel; }
-    }
-    player_x_pos += player_x_vel;
-    player_y_pos += player_y_vel;
+
+    uint16_t Tx,Ty;
+    Tx=player_x_pos;
+    Ty=player_y_pos;
+
+    uint8_t buttons = joypad();
+
+    // gravity
+    player_y_vel += 3;
+    if (player_y_vel> 500) player_y_vel = 500;
 
 
-    player_sprite_x = player_x_pos>>8;
-    player_sprite_y = player_y_pos>>8;
-    move_player_sprite(player_sprite_x,player_sprite_y);
+    if (buttons && J_UP){
+        if (jumpflag){
+            player_y_vel += -500;
+            jumpflag=FALSE;
+        }
+    }
+
+    if (!(buttons && J_UP)){
+        jumpflag=TRUE;
+    }
+
+    EMU_printf("pyv %d",player_y_vel);
+    Ty += player_y_vel;
+    Tx += player_x_vel;
+    // Check for collisions with platforms
+    uint8_t SX,SY,T;
+    uint8_t x,y;
+    x = Tx>>8;
+    y = Ty>>8;
+    SX = sprite_x_to_tile_x(x);
+    SY = sprite_y_to_tile_y(y);
+    SY +=2;
+    T = get_bkg_tile_xy(SX,SY);
+
+    if (T!=1){
+        player_y_pos = Ty;
+    } else {
+        //player_y_vel=0;
+    }
+    player_x_pos = Tx;
+
+    
+
+    //EMU_printf("BB %d,%d  %d",SX,SY,T);
+
+
+    move_player_sprite(player_x_pos>>8,player_y_pos>>8);
+
 
 }
 
@@ -87,32 +121,12 @@ void main(void)
 
     // Loop forever
     while(1) {
-        switch(joypad()){
-            case J_RIGHT:
-                player_x_vel = maxvel;
-                break;
-            case J_LEFT:
-                player_x_vel = -maxvel;
-                break;
-            case J_UP:
-                player_y_vel = -maxvel;
-                break;
-            case J_DOWN:
-                player_y_vel = maxvel;
-                break;
-            default:
-                player_x_vel=0;
-                player_y_vel=0;
-        }
+
+
 
 		// Game main loop processing goes here
         handle_player_sprite_physics();
-        uint8_t SX,SY,T;
-        SX = sprite_x_to_tile_x(player_sprite_x);
-        SY = sprite_y_to_tile_y(player_sprite_y);
-        T = get_bkg_tile_xy(SX,SY);
 
-        EMU_printf("BB %d,%d  %d",SX,SY,T);
 
 		// Done processing, yield CPU and wait for start of next frame
         wait_vbl_done();
